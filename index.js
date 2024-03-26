96e27e586c709362bd1d2cb96c50428f2fc72f6c
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-03-26 08:17:15 +0300

    initiall commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (namesArr){
+ 
+  return namesArr.slice(0, 2);
+};
+const returnLastTwoDrivers = function (namesArr){
+  return namesArr.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function (int){
+  return (fare) => int * fare;
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(namesArr, driversToReturn){
+  return driversToReturn(namesArr);
+};
+
